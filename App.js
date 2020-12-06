72b714e2acb9d57d9b1c414819e3641892015502
Arbaz Yousuf <contactme797@gmail.com>
2020-12-06 14:52:59 +0500

    setting error

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import Stack from './src/navigations/stacknavigation';
-import BGcustom from './src/components/bgcustom';
-import Toast from 'react-native-toast-message';
+// import Stack from './src/navigations/stacknavigation';
+// import BGcustom from './src/components/bgcustom';
+// import Toast from 'react-native-toast-message';
+import Tutorial from './src/screens/tutorial';
+import {View} from 'react-native';
 const App = (props) => {
   return (
-    // <BGcustom />
-    <>
-      <Stack navigation={props.navigation} />
-      <Toast ref={(ref) => Toast.setRef(ref)} />
-    </>
+    <View style={{flex: 1}}>
+      <Tutorial />
+    </View>
   );
 };
 
