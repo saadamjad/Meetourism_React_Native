a0714530d6739567e5f89b790d47a5c00afd3ad6
Arbaz Yousuf <contactme797@gmail.com>
2020-12-06 15:12:24 +0500

    splash screen done

--- src/screens/tutorial/index.tsx
+++ src/screens/tutorial/index.tsx
@@ -5,97 +5,98 @@ import {Icon} from 'native-base';
 import CustomView from '../../components/customView';
 import {theme} from '../../constants/theme';
 
-const firstSlide = () => (
-  <View
-    style={{
-      height: '50%',
-      // backgroundColor: 'black',
-      width: '100%',
-      justifyContent: 'center',
-    }}>
-    <View style={{width: '100%', height: 220, alignItems: 'flex-end'}}>
-      <View
-        style={{
-          width: '80%',
-          backgroundColor: theme.primaryColor,
-          // paddingVertical: 50,
-          height: 180,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderBottomLeftRadius: 100,
-        }}>
-        <Text style={{color: 'white', fontSize: 34, fontWeight: '800'}}>
-          Welcome to Meetourism
-        </Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: theme.secondaryColor,
-          width: '45%',
-          position: 'absolute',
-          height: 60,
-          bottom: 0,
-          borderTopLeftRadius: 30,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderBottomLeftRadius: 30,
-          flexDirection: 'row',
-        }}>
-        <Text style={{color: 'white', fontWeight: '300', fontSize: 18}}>
-          Get Started
-        </Text>
-        {/* <Icon name="arrowright" type="AntDesign" /> */}
-      </View>
-    </View>
-  </View>
-);
-const secondSlide = () => (
-  <View
-    style={{
-      height: '50%',
-      // backgroundColor: 'black',
-      width: '100%',
-      justifyContent: 'center',
-    }}>
-    <View style={{width: '100%', height: 220, alignItems: 'flex-end'}}>
-      <View
-        style={{
-          width: '80%',
-          backgroundColor: theme.primaryColor,
-          // paddingVertical: 50,
-          height: 180,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderBottomLeftRadius: 100,
-        }}>
-        <Text style={{color: 'white', fontSize: 34, fontWeight: '800'}}>
-          Welcome to Meetourism
-        </Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: theme.secondaryColor,
-          width: '45%',
-          position: 'absolute',
-          height: 60,
-          bottom: 0,
-          borderTopLeftRadius: 30,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderBottomLeftRadius: 30,
-          flexDirection: 'row',
-        }}>
-        <Text style={{color: 'white', fontWeight: '300', fontSize: 18}}>
-          Get Started
-        </Text>
-        {/* <Icon name="arrowright" type="AntDesign" /> */}
-      </View>
-    </View>
-  </View>
-);
-
 const Tutorial = (props) => {
   const [state, setState] = useState({selectedIndex: 0});
+
+  const firstSlide = () => (
+    <View
+      style={{
+        height: '50%',
+        // backgroundColor: 'black',
+        width: '100%',
+        justifyContent: 'center',
+      }}>
+      <View style={{width: '100%', height: 210, alignItems: 'flex-end'}}>
+        <View
+          style={{
+            width: '80%',
+            backgroundColor: theme.primaryColor,
+            // paddingVertical: 50,
+            height: 180,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderBottomLeftRadius: 100,
+          }}>
+          <Text style={{color: 'white', fontSize: 34, fontWeight: '800'}}>
+            Welcome to Meetourism
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={() => setState({...state, selectedIndex: 1})}
+          style={{
+            backgroundColor: theme.secondaryColor,
+            width: '45%',
+            position: 'absolute',
+            height: 60,
+            bottom: 0,
+            borderTopLeftRadius: 30,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderBottomLeftRadius: 30,
+            flexDirection: 'row',
+          }}>
+          <Text style={{color: 'white', fontWeight: '300', fontSize: 18}}>
+            Get Started
+          </Text>
+          {/* <Icon name="arrowright" type="AntDesign" /> */}
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+  const secondSlide = () => (
+    <View
+      style={{
+        height: '30%',
+        // backgroundColor: 'black',
+        width: '100%',
+        justifyContent: 'center',
+      }}>
+      <View
+        style={{
+          width: '100%',
+          height: 220,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            backgroundColor: theme.secondaryColor,
+            width: 160,
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 45,
+            borderRadius: 30,
+          }}>
+          <Text style={{color: theme.textColor.whiteColor, fontSize: 20}}>
+            Avail Offer
+          </Text>
+        </View>
+        <View
+          style={{
+            width: 140,
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 60,
+          }}>
+          <Text style={{color: theme.textColor.whiteColor, fontSize: 13}}>
+            Skip to Login
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+
   return (
     <CustomView>
       <View style={{flex: 1, justifyContent: 'space-between'}}>
