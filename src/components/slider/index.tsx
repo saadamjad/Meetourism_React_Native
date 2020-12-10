50ca5dda2801d8f8a2c783381ec4c33810e5422d
MoizYousuf <moizyousuf24@gmail.com>
2020-12-10 20:34:20 +0500

    add one screen

---
+++ src/components/slider/index.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Slider from 'react-native-slider';
+import {theme} from '../../constants/theme';
+const SliderCom = (props) => {
+  return (
+    <>
+      <View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Text
+            style={{
+              color: theme.textColor.blackColor,
+              fontSize: 14,
+              marginVertical: 10,
+            }}>
+            {' '}
+            English{' '}
+          </Text>
+          <Text
+            style={{
+              color: theme.textColor.blackColor,
+              fontSize: 14,
+              marginVertical: 10,
+            }}>
+            {' '}
+            Native{' '}
+          </Text>
+        </View>
+
+        <Slider
+          onValueChange={(value) => console.log('value', value)}
+          thumbStyle={{backgroundColor: '#D9B372'}}
+          trackStyle={{
+            backgroundColor: theme.textColor.blackColor,
+            borderRadius: 10,
+          }}
+          thumbTintColor="#0000"
+        />
+      </View>
+
+      <View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Text
+            style={{
+              color: theme.textColor.blackColor,
+              fontSize: 14,
+              marginVertical: 10,
+            }}>
+            {' '}
+            Russian{' '}
+          </Text>
+          <Text
+            style={{
+              color: theme.textColor.blackColor,
+              fontSize: 14,
+              marginVertical: 10,
+            }}>
+            {' '}
+            Fluient{' '}
+          </Text>
+        </View>
+
+        <Slider
+          // value={this.state.value}
+          onValueChange={(value) => console.log('value', value)}
+          thumbStyle={{
+            backgroundColor: '#D9B372',
+            height: 20,
+            width: 20,
+            borderRadius: 3,
+          }}
+          trackStyle={{
+            backgroundColor: theme.textColor.blackColor,
+            borderRadius: 10,
+          }}
+          // style={{backgroundColor:'blue'}}
+          thumbTintColor="#0000"
+        />
+      </View>
+    </>
+  );
+};
+export default SliderCom;
