cd3bd8620a5e897f5c56799982a68535c9576223
MoizYousuf <moizyousuf24@gmail.com>
2021-07-13 01:37:45 +0500

    set hy boss

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import Stack from './src/navigations/stacknavigation';
 // import BGcustom from './src/components/bgcustom';
@@ -13,21 +13,52 @@ import SelectOffer from './src/screens/alloffers/selectOffer';
 import DetailOffer1 from './src/screens/alloffers/detailsOffer1';
 // import History from './src/screens/alloffers/history';
 
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './src/redux/store';
-import { Provider } from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './src/redux/store';
+import {Provider} from 'react-redux';
 // import { translate } from 'react-i18next';
-import { withTranslation } from 'react-i18next';
+import {withTranslation} from 'react-i18next';
+import OneSignal from 'react-native-onesignal';
 // import './src/I18n'
 
-
-
 const App = (props) => {
-
   const _Stack = () => {
-    return <Stack navigation={props.navigation} />
+    return <Stack navigation={props.navigation} />;
+  };
+  useEffect(() => {
+    OneSignal.setLogLevel(6, 0);
+    OneSignal.setAppId('7d35068c-2c86-4a49-bc6b-b8d38d5c2f05');
+    OneSignal.getDeviceState().then((data) => {
+      console.log("GOT THE DATA ! " ,data)
+    })
+    //END OneSignal Init Code
 
-  }
+    //Prompt for push on iOS
+    // OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+    //   console.log('Prompt response:', response);
+    // });
+
+    //Method for handling notifications received while app in foreground
+    OneSignal.setNotificationWillShowInForegroundHandler(
+      (notificationReceivedEvent) => {
+        console.log(
+          'OneSignal: notification will show in foreground:',
+          notificationReceivedEvent,
+        );
+        let notification = notificationReceivedEvent.getNotification();
+        console.log('notification: ', notification);
+        const data = notification.additionalData;
+        console.log('additionalData: ', data);
+        // Complete with null means don't show a notification.
+        notificationReceivedEvent.complete(notification);
+      },
+    );
+
+    //Method for handling notifications opened
+    OneSignal.setNotificationOpenedHandler((notification) => {
+      console.log('OneSignal: notification opened:', notification);
+    });
+  }, []);
   // const ReloadAppOnLanguageChange = withTranslation('translation', {
   //   bindI18n: 'languageChanged',
   //   bindStore: false,
@@ -46,7 +77,6 @@ const App = (props) => {
 };
 
 export default App;
-
 
 // import React from 'react';
 // import {translate } from 'react-i18next';
