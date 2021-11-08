61c6aba036dc56b1406c03552992eec3085dea05
MoizYousuf <projectskahoodigitals@gmail.com>
2021-11-08 21:13:00 +0500

    update ios notification

--- App.js
+++ App.js
@@ -19,47 +19,35 @@ const App = (props) => {
   };
   useEffect(() => {
     OneSignal.setLogLevel(6, 0);
-    OneSignal.setAppId('7d35068c-2c86-4a49-bc6b-b8d38d5c2f05');
-    OneSignal.getDeviceState().then((data) => {
-      console.log("GOT THE DATA ! ", data)
-    })
+    OneSignal.setAppId("7d35068c-2c86-4a49-bc6b-b8d38d5c2f05");
     //END OneSignal Init Code
-
+    
     //Prompt for push on iOS
-    // OneSignal.promptForPushNotificationsWithUserResponse((response) => {
-    //   console.log('Prompt response:', response);
-    // });
-
+    OneSignal.promptForPushNotificationsWithUserResponse(response => {
+      console.log("Prompt response:", response);
+    });
+    
     //Method for handling notifications received while app in foreground
-    OneSignal.setNotificationWillShowInForegroundHandler(
-      (notificationReceivedEvent) => {
-        console.log(
-          'OneSignal: notification will show in foreground:',
-          notificationReceivedEvent,
-        );
-        let notification = notificationReceivedEvent.getNotification();
-        console.log('notification: ', notification);
-        const data = notification.additionalData;
-        console.log('additionalData: ', data);
-        // Complete with null means don't show a notification.
-        notificationReceivedEvent.complete(notification);
-      },
-    );
-
+    OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+      console.log("OneSignal: notification will show in foreground:", notificationReceivedEvent);
+      let notification = notificationReceivedEvent.getNotification();
+      console.log("notification: ", notification);
+      const data = notification.additionalData
+      console.log("additionalData: ", data);
+      // Complete with null means don't show a notification.
+      notificationReceivedEvent.complete(notification);
+    });
+    
     //Method for handling notifications opened
-    OneSignal.setNotificationOpenedHandler((notification) => {
-      console.log('OneSignal: notification opened:', notification);
+    OneSignal.setNotificationOpenedHandler(notification => {
+      console.log("OneSignal: notification opened:", notification);
     });
   }, []);
-  // const ReloadAppOnLanguageChange = withTranslation('translation', {
-  //   bindI18n: 'languageChanged',
-  //   bindStore: false,
-  // })(_Stack);
+
   return (
     <>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          {/* <ReloadAppOnLanguageChange /> */}
           {_Stack()}
           <Toast ref={(ref) => Toast.setRef(ref)} />
         </PersistGate>
---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Macbook on 07/11/2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
